4911eefd67a8fd150d5d3f3f381981c0ef84deab
Fabrice Etiennette <fabrice.etiennette@hotmail.com>
2019-05-25 00:25:53 +0200

    main algo and creation of Detail console design

--- Consoles Retro/HomeController.swift
+++ Consoles Retro/HomeController.swift
@@ -13,20 +13,68 @@ class HomeController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var retroIV: UIImageView!
     
-    
+    var consoles: [Console] = []
+    var index = 0
+    var segueID = "Detail"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        consoles = ConsoleCollection().obtenirListe()
+         let tap = UITapGestureRecognizer(target: self, action: #selector(toDetail))
+        retroIV.isUserInteractionEnabled = true
+        retroIV.addGestureRecognizer(tap)
+        setup()
 
     }
     
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    @objc func toDetail() {
+        performSegue(withIdentifier: segueID, sender: nil)
+        
+    }
+    
+    
+    
+    func setup() {
+        let console = consoles[index]
+        titleLabel.text = "Mes Consoles Retro\n" + console.nom
+        retroIV.image = console.image
+        view.backgroundColor = console.couleur
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == segueID {
+            if let controller = segue.destination as? DetailController {
+                controller.console = consoles[index]
+            }
+        }
+    }
+    
     @IBAction func previousPressed(_ sender: Any) {
+        if index == 0 {
+            index = consoles.count - 1
+        } else {
+            index -= 1
+        }
+        setup()
     }
     
     @IBAction func nextPressed(_ sender: Any) {
+        if index == consoles.count - 1 {
+            index = 0
+        } else {
+            index += 1
+        }
+        setup()
     }
     
     @IBAction func randomPressed(_ sender: Any) {
+        index = Int.random(in: 0..<consoles.count)
+        setup()
     }
 }
