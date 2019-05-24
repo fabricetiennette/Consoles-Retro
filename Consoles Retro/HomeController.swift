bbea00e6b7c74d688e6a09406fc8b1d16ae13691
Fabrice Etiennette <fabrice.etiennette@hotmail.com>
2019-05-24 12:17:06 +0200

    Add assets and first controller design

--- Consoles Retro/HomeController.swift
+++ Consoles Retro/HomeController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeController.swift
 //  Consoles Retro
 //
 //  Created by Fabrice Etiennette on 24/05/2019.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    }
+    
 
 }
-
